691e662aa4180f6d3f3148bd71f80d95ba493d68
James Wang <jmaeswnag@gmail.com>
2024-05-27 18:25:30 -0500

    Completed calculator interface and created basic logic for the number buttons and display.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", (e) => {
     console.log("DOM fully loaded and parsed, running script");
     //**************************************************
-    let num1 = undefined;
-    let num2 = undefined;
-    let op = undefined;
+    let num1 = null;
+    let num2 = null;
+    let op = null;
+    let currDisplay = "";
+    const DISPLAY_LIMIT = 10 ** 22;
 
     function operate(num1, op, num2) {
         if (typeof num1 != "number" || 
@@ -74,7 +76,63 @@ document.addEventListener("DOMContentLoaded", (e) => {
         const rows = document.querySelectorAll(".buttons-row");
         rows.forEach(makeRowsSquare);
     }
+
     
+
+    function initializeNumbers() {
+        for(let i = 0; i < 10; i++) {
+
+            function numberClicked(e) {
+                /*When a number is clicked:
+                 1. If there is no operator on display, add another digit to num1
+                    Nothing else needs to be done in this case
+                 2. If there is a operator on display add another digit to num2
+                    Should also signal the calculator to display num2 rather
+                    than num1*/ 
+                if(op === null) {
+                    /*
+                    if (num1 < DISPLAY_LIMIT) {
+                        num1 = num1 * 10 + i;
+                        currDisplay = num1;
+                    }
+                    */
+                    num1 = num1 * 10 + i;
+                    currDisplay = num1;
+                }
+                else {
+                    /*
+                    if (num2 < DISPLAY_LIMIT) {
+                        num2 = num2 * 10 + i;
+                        currDisplay = num2;
+                    }
+                    */
+                    num2 = num2 * 10 + i;
+                    currDisplay = num2;
+                }
+                console.log(`num1 : ${num1}, num2 : ${num2}`);
+            }
+            //Get the name of the corresponding tag query
+            let query = ".button-" + i;
+            const button = document.querySelector(query);
+            button.addEventListener("click", numberClicked);
+        }
+    }
+    
+    function initializeDisplay() {
+        function refreshDisplay(e) {
+            const displayNumbers = document.querySelector(".display-numbers");
+            displayNumbers.textContent = currDisplay;
+        }
+        const body = document.querySelector("body");
+        body.addEventListener("click", refreshDisplay)
+    }
+    
+
     makeButtonsSquare();
+    initializeNumbers();
+    initializeDisplay();
+    initializeOperators();
+    initializeEvalButton();
+    initializeClear();
 
 });
